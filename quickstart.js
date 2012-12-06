347f48107dca8f3afde40d664910e14eeacd7a00
michael <michaelc@zoovy.com>
2012-12-06 10:34:19 -0800

    Featured Product tweaks
    Added bg color to images, removed QuickView functionality, changed
    layout.  See productListTemplateNoQuickview for image bgcolor example.

--- quickstart.js
+++ quickstart.js
@@ -32,6 +32,7 @@ var myRIA = function() {
 			'categoryListTemplate',
 			'categoryListTemplateRootCats',
 			'productListTemplate',
+			'productListTemplateNoQuickview',
 			'productListTemplateATC',
 			'productListTemplateBuyerList',
 			'productListTemplateResults',
