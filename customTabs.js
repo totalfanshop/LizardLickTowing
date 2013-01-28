dfd2b972c5914487d45d1a4991f4d2b52709267a
Michael Chance <michaelc@zoovy.com>
2013-01-28 08:34:43 -0800

    Custom Tabs "app://" removed

--- customTabs.js
+++ customTabs.js
@@ -26,6 +26,9 @@ else{/*do nothing, can't warn if console.warn isn't defined*/}
 
 function isLocal( anchor ) {
 	//alert(anchor.href.replace( rhash, "" )+".."+location.href.replace( rhash, "" ).replace( /\s/g, "%20" ));
+	
+	return true;
+	/*
 	if(anchor.href.replace( rhash, "" ) === "app://"){
 		//app.u.dump("bypassing isLocal check");
 		return true;
@@ -38,7 +41,7 @@ function isLocal( anchor ) {
 				// Safari 5.1 doesn't encode spaces in window.location
 				// but it does encode spaces from anchors (#8777)
 				.replace( /\s/g, "%20" );
-	
+	*/
 }
 
 $.widget( "ui.localtabs", {
